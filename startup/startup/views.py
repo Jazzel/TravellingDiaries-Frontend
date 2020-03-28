fc8207435636824b47557dcde1d31e27f97b0c01
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2020-03-28 15:42:16 +0500

    still working on homepage

--- startup/startup/views.py
+++ startup/startup/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import TemplateView
+from destinations.models import Destination, DestinationImage
+from cities_light.models import City
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        city = City.objects.get(name='Karachi')
+        destination = Destination.objects.filter(city=city).order_by(
+            '-created').first()
+        default_image = DestinationImage.objects.filter(
+            destination=destination).first()
+        print(default_image.image)
+        context['city_data'] = destination
+        return context
