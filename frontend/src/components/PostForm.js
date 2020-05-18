4a5e88a96a82de72ea02d2b4e97c175009a1b13b
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2020-05-19 02:42:46 +0500

    Home page almost reactified - Working on posts now

---
+++ frontend/src/components/PostForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Card, Row, Col, Form, Input } from "reactstrap";
+
+const PostForm = () => {
+  return (
+    <Card style={{ zIndex: 99 }}>
+      <div className="w-100">
+        <Row className="p-3">
+          <Col className="col-1">
+            <img
+              className="rounded-circle"
+              width="50"
+              src="./../../public/assets/images/pp.jpeg"
+              alt="user-image"
+            />
+          </Col>
+          <Col className="col ml-1">
+            <Form>
+              <Input
+                className="form-control m-0 w-100"
+                style={{ fontSize: 25 }}
+                type="text"
+                placeholder="What's up ? Jazz"
+              />
+            </Form>
+          </Col>
+        </Row>
+        <Row className="p-3 text-center">
+          <Col>
+            <i
+              className="text-primary fas fa-user-friends"
+              aria-hidden="true"
+            ></i>{" "}
+            &nbsp; Tag Someone
+          </Col>
+          <Col>
+            <i
+              className="text-primary fas fa-user-friends"
+              aria-hidden="true"
+            ></i>{" "}
+            &nbsp; Tag Someone
+          </Col>
+          <Col>
+            <i
+              className="text-primary fas fa-user-friends"
+              aria-hidden="true"
+            ></i>{" "}
+            &nbsp; Tag Someone
+          </Col>
+        </Row>
+      </div>
+    </Card>
+  );
+};
+
+export default PostForm;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,87 @@
 import React from "react";
 import Navigation from "../containers/Navigation";
 import SearchBar from "../containers/SearchBar";
+import MenuButton from "../components/MenuButton";
+import "./Home.css";
+import { Card, CardHeader, CardBody, CardImg } from "reactstrap";
+import { Row, Col } from "reactstrap";
+import PostForm from "../components/PostForm";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Navigation />
       <SearchBar />
+      <Card>
+        <CardHeader className="headImage"></CardHeader>
+        <CardBody className="headCard w-100">
+          <Row>
+            <Col className="col-3">
+              <Card className="border-0 shadow locationCard">
+                <CardImg
+                  top
+                  src={
+                    "http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/02n.png"
+                  }
+                  style={{ zIndex: -1 }}
+                />
+                <CardBody className="bg-light">
+                  <h4 className="card-title">
+                    <p>
+                      <i className="fa fa-map-marker-alt float-left"></i>
+                      &nbsp; Karachi, Sindh, Pakistan
+                    </p>
+                  </h4>
+                  <p>
+                    Featured destinations:
+                    <br />
+                    <span className="badge badge-dark">Destination 3</span>
+                    <span className="badge badge-dark">Destination 2</span>
+                    <span className="badge badge-dark">Destination 1</span>
+                  </p>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col className="col-6">
+              <MenuButton />
+              <br />
+              <br />
+              <PostForm />
+              <div class="container-fluid p-0 m-0">
+                <h2 className="mt-5 ml-3">Selected: Attractions ........</h2>
+                <hr />
+                <h2 className="mt-5 ml-3">Trending near you ........</h2>
+                <hr />
+                <div className="p-0" id="post-section"></div>
+              </div>
+            </Col>
+            <Col className="col-3">
+              <Card className="border-0 shadow weatherCard">
+                <CardImg
+                  top
+                  src={
+                    "http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/02n.png"
+                  }
+                  style={{ zIndex: -1 }}
+                />
+                <CardBody className="bg-light">
+                  <h4 className="card-title">
+                    <p>
+                      <img
+                        src="http://openweathermap.org/img/w/02n.png"
+                        alt="Image"
+                      />
+                      &nbsp; Weather - 30° C
+                    </p>
+                  </h4>
+                  <hr />
+                  <p>Few clouds - Good Evening !!</p>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
     </React.Fragment>
   );
 };
